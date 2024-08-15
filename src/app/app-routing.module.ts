d7ff979053c684144911b0dacad21cb5ec78da12
Kenryan Luke <113695261+einnnnn@users.noreply.github.com>
2024-08-15 22:08:59 +0400

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,10 @@ import { LandingPageComponent } from './home/landing-page/landing-page.component
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
-  { path: '', component: LandingPageComponent },
+  { path: 'home', component: LandingPageComponent },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
-  },
-  {
-    path: 'about',
-    loadChildren: () =>
-      import('./about/about.module').then((m) => m.AboutModule),
-  },
-  {
-    path: 'projects',
-    loadChildren: () =>
-      import('./projects/projects.module').then((m) => m.ProjectsModule),
-  },
-  {
-    path: 'contact',
-    loadChildren: () =>
-      import('./contact/contact.module').then((m) => m.ContactModule),
   },
 ];
 
