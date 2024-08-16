b5c898f51fe7f5d61b85da5d2eedcaf608c77204
kenryanlukearias <studyholicprocrastinator@gmail.com>
2024-08-16 09:19:40 +0400

    updates

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,6 @@ import { HomeRoutingModule } from './home-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { ContactModule } from '../contact/contact.module';
-import { AboutModule } from '../about/about.module';
-import { ProjectsModule } from '../projects/projects.module';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @NgModule({
@@ -17,9 +14,7 @@ import { NzGridModule } from 'ng-zorro-antd/grid';
     HomeRoutingModule,
     NzLayoutModule,
     NzIconModule,
-    ContactModule,
-    AboutModule,
-    ProjectsModule,
+
     NzGridModule,
   ],
 })
