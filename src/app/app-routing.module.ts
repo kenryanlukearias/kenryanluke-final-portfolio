81355d18a1d15bead9b01e4e5235930327887457
Kenryan Luke <113695261+einnnnn@users.noreply.github.com>
2024-08-20 14:12:51 +0400

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,21 +9,6 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
-  {
-    path: 'about',
-    loadChildren: () =>
-      import('./about/about.module').then((m) => m.AboutModule),
-  },
-  {
-    path: 'projects',
-    loadChildren: () =>
-      import('./projects/projects.module').then((m) => m.ProjectsModule),
-  },
-  {
-    path: 'contact',
-    loadChildren: () =>
-      import('./contact/contact.module').then((m) => m.ContactModule),
-  },
 ];
 
 @NgModule({
