f7f61407e3e61caaebd0439d05b07a3bba4b821b
Kenryan Luke <113695261+einnnnn@users.noreply.github.com>
2024-08-20 14:31:04 +0400

    changes

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [{ path: 'home', component: LandingPageComponent }];
+const routes: Routes = [{ path: '', component: LandingPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
