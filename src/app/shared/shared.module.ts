1c024e63ca04e8bfc41c8acfdcae823e128c1506
Kenryan Luke <113695261+einnnnn@users.noreply.github.com>
2024-08-20 14:24:03 +0400

    delete unnecassary modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent],
   imports: [CommonModule, SharedRoutingModule, NzMenuModule, NzIconModule],
-  exports: [NzIconModule]
+  exports: [NzIconModule],
 })
 export class SharedModule {}
